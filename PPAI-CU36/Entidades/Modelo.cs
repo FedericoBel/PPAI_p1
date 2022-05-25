193e4b6dead2fee75e30be4e4d5662582375d26a
ErikSalva <erikshalva@gmail.com>
2022-05-25 11:55:39 -0300

    Update Modelo
    Cambio en la entidad modelo, constructor y metodos de seteo

--- PPAI-CU36/Entidades/Modelo.cs
+++ PPAI-CU36/Entidades/Modelo.cs
@@ -8,8 +8,30 @@ namespace PPAI_CU36.Entidades
 {
     public class Modelo
     {
-        public int idModelo { get; set; }
-        public string nombre { get; set; }
-        public int idMarca { get; set; }
+        private int idModelo;
+        private string nombre;
+        private int idMarca;
+        public Modelo(int idmodelo, string Nombre, int idmarca)
+        {
+            idModelo = idmodelo;
+            nombre = Nombre;
+            idMarca = idmarca;
+        }
+
+        public int idDeModelo
+        {
+            get => idModelo;
+            set => idModelo = value;
+        }
+        public string nombreDeModelo
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+        public int idDeMarca
+        {
+            get => idMarca;
+            set => idMarca = value;
+        }
     }
 }
